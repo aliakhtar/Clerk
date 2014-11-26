65721ab7ef9cd8ed21416d51868bd299f396f001
Ali <ali.rac200@gmail.com>
2014-11-26 09:46:34 +0500

    Refactored test.

--- src/test/java/com/github/aliakhtar/clerk/util/CodeGenTest.java
+++ src/test/java/com/github/aliakhtar/clerk/util/CodeGenTest.java
@@ -4,18 +4,19 @@ import com.github.aliakhtar.annoTest.annotation.PrintMe;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @PrintMe
 public class CodeGenTest
 {
     @Test
-    public void readWorks()
+    public void testGetTemplate()
             throws Exception
     {
         String template =
                 CodeGen.get().getTemplate(this.getClass(), "SimpleBean.vm");
 
         assertNotNull(template, template);
-        System.out.println(template);
+        assertTrue(template, template.contains("@NotNull") );
     }
 }
